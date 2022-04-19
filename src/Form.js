7310b562d677719b5595bf06eddf27d7e7a2c706
Ayman Ismail <ayman@Aymans-MacBook-Pro-2.local>
2022-04-19 12:22:49 -0400

    updated form page

--- src/Form.js
+++ src/Form.js
@@ -1,160 +1,246 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import form from "react-bootstrap/Form";
-// import { Link } from 'react-router';
-// import Swal from 'sweetalert2'
+import axios from 'axios'
+
 import "./Form.css";
 import { Col, FormGroup } from "react-bootstrap";
 
+class Form extends React.Component {
+  
+  // const [slope, setSlope] = useState("");
+  // const [ca, setCa] = useState("");
+  // const [thal, setThal] = useState("");
 
-function Form() {
-  const [Age, setAge] = useState("");
-  const [Sex, setSex] = useState("");
-  const [CP, setCP] = useState("");
-  const [TrestBps, setTrestBps] = useState("");
-  const [Cholesterol, setCholesterol] = useState("");
-  const [FBS, setFbs] = useState("");
-  const [restecg, setRestEcg] = useState("");
-  const [thalach, setThalach] = useState("");
-  const [exang, setExang] = useState("");
-  const [oldPeak, setOldPeak] = useState("");
-  const [slope, setSlope] = useState("");
-  const [ca, setCa] = useState("");
-  const [thal, setThal] = useState("");
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="background">
-      <div className="Form">
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Age
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Your Age(26 - 62).." />
-          </Col>
-        </form.Group>
+    this.state = {
+      isLoading: false,
+      formData: {
+        "age": 0,
+        "sex": 0,
+        "cp": 0,
+        "trestbps": 0,
+        "cholestrol": 0,
+        "fbs": 0,
+        "restecg": 0,
+        "thalach": 0,
+        "exang": 0,
+        "oldpeak": 0
+      },
+      result: ""
+    };
+  }
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-          <form.Label column sm="2">
-            Sex
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    var formData = this.state.formData;
+    formData[name] = value;
+    this.setState({
+      formData
+    });
+  }
+
+  handlePredictClick = (event) => {
+    const formData = this.state.formData;
+    console.log(formData);
+    this.setState({ isLoading: true });
+    fetch('http://127.0.0.1:5000/prediction/', 
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          result: response.result,
+          isLoading: false
+        });
+      });
+  }
+
+  // const [Age, setAge] = useState("");
+  // const [Sex, setSex] = useState("");
+  // const [CP, setCP] = useState("");
+  // const [TrestBps, setTrestBps] = useState("");
+  // const [Cholesterol, setCholesterol] = useState("");
+  // const [FBS, setFbs] = useState("");
+  // const [restecg, setRestEcg] = useState("");
+  // const [thalach, setThalach] = useState("");
+  // const [exang, setExang] = useState("");
+  // const [oldPeak, setOldPeak] = useState("");
+
+
+  render() {
+    const isLoading = this.state.isLoading;
+    const formData = this.state.formData;
+    const result = this.state.result;
+
+    return (
+      <div className="background">
+        <div className="Form">
+        <form>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              Age
             </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="0 For Male and 1 For Female.." />
-          </Col>
-        </form.Group>
+            <Col sm="10">
+              <form.Control name="age" type="text" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Chest Pain
-          </form.Label>
-          <Col sm="5">
-            <form.Control type="text" placeholder="1:Typical, 2:Atypical, 3:Non-Anginal, 4:Asymptomatic" />
-          </Col>
-        </form.Group>
+          {/* <form.Group as={Row} className="mb-3" controlId="formPlaintextPassword" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              Sex
+            </form.Label>
+            <form.Check
+              type="radio"
+              label="1"
+              name="formHoriRadios"
+            />
+            <form.Check
+              type="radio"
+              label="0"
+              name="formHoriRadios"
+            />
+          </form.Group> */}
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Trestbps
-          </form.Label>
-          <Col sm="5">
-            <form.Control type="text" placeholder="Resting BP.." />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              sex
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="sex" type="text" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Cholesterol
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Cholesterol Levels.." />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              cp
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="cp" type="text" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            FBS
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Fasting BP.. (1 = True, 0 = False)" />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              trestbps
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="trestbps" type="text" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            RestECG
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="0:Normal, 1:ST-T, 2-Hypertrophy" />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              chol
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="cholestrol" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Thalach
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Maximum Heart Rate Achieved.." />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              fbs
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="fbs" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Exang
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Exercise Induced Anigma..(1: Yes, 0: No)" />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              restecg
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="restecg" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Old Peak
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="ST Depression Induced by Exercise Relative to Rest.." />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              thalach
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="thalach" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Slope
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="1:Upsloping, 2: Flat, 3:Downsloping" />
-          </Col>
-        </form.Group>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              exang
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="exang" />
+            </Col>
+          </form.Group>
 
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            CA
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="Vessels Colored by Flouroslopy (0.3).."/>
-          </Col>
-        </form.Group>
-
-        <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <form.Label column sm="2">
-            Thalassemia
-          </form.Label>
-          <Col sm="5">
-            <form.Control placeholder="3:normal, 6:Fixed Defect, 7-Reversable Defect" />
-          </Col>
-        </form.Group>
-
-        {/* <Button className="button-predict">
-         Predict
-        </Button> */}
-      </div>
-      <a href ="landing">
-      <Button className="button-predict">
-         Predict
-        </Button>
-        </a>
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={this.handleChange}>
+            <form.Label column sm="2">
+              old peak
+            </form.Label>
+            <Col sm="10">
+              <form.Control name="oldpeak"/>
+            </Col>
+          </form.Group>
+          
         
-    </div>
-  );
+            
+
+          {/* <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={(e) => setSlope(e.target.value)}>
+            <form.Label column sm="2">
+              slope
+            </form.Label>
+            <Col sm="10">
+              <form.Control />
+            </Col>
+          </form.Group>
+
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" setCa>
+            <form.Label column sm="2">
+              ca
+            </form.Label>
+            <Col sm="10">
+              <form.Control />
+            </Col>
+          </form.Group>
+
+          <form.Group as={Row} className="mb-3" controlId="formPlaintextEmail" onChange={(e) => setThal(e.target.value)}>
+            <form.Label column sm="2">
+              thal
+            </form.Label>
+            <Col sm="10">
+              <form.Control />
+            </Col>
+          </form.Group> */}
+
+          <Button
+                  block
+                  variant="success"
+                  disabled={isLoading}
+                  onClick={!isLoading ? this.handlePredictClick : null}>
+                  { isLoading ? 'Making prediction' : 'Predict' }
+          </Button>
+          </form>
+          {result === "" ? null :
+              (<Row>
+                <Col className="result-container">
+                  <h5 id="result">{result}</h5>
+                </Col>
+              </Row>)
+            }
+        </div>
+      </div>
+    );
+  }
 }
 export default Form;
-
